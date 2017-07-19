73786514bae05180eb009b3df8f51895118d7375
daniel-certa-1228 <mfdancerta@gmail.com>
2017-07-18 23:19:40 -0500

    finished MVP, just needs some tweeks

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ function checkNumField(number) {
 	}  else {
 		return true;
 	}
-
-}
+};
 
 function checkCharField(character) { 
 	if (character === "") {
@@ -15,33 +14,65 @@ function checkCharField(character) {
 	}  else {
 		return true;
 	}
-}
+};
 
 //gets the number from the number field
 function getNumber() {
 	let userNumber = document.getElementById("tree_number").value;
-	if (checkNumField(userNumber)){
-		userNumber = Number(userNumber);
-		console.log( "userNumber", userNumber );
-		return userNumber
-	} else  {
-		alert("Both fields must be filled");
-	}
-}
+	userNumber = Number(userNumber);
+	// console.log( "userNumber", userNumber );
+	return userNumber
+};
+
 //gets the character from the charater field
 function getCharacter() {
 	let userCharacter = document.getElementById("tree_character").value;
-	if (checkCharField(userCharacter)) {
-		userCharacter = userCharacter.toString();
-		console.log( "userCharacter", userCharacter );
-		return userCharacter
-	} else {
-		alert("Both fields must be filled");
+	userCharacter = userCharacter.toString();
+	// console.log( "userCharacter", userCharacter );
+	return userCharacter
+
+};
+
+// define the tree object and fill with the appropriate data
+//throw an error when one or both of the fields aren't filled in
+function fillTreeObject() {
+	let treeObject = {};
+	let userHeight = getNumber();
+	let userCharacter = getCharacter();
+	let numFieldFilled = checkNumField(userHeight);
+	let charFieldFilled = checkCharField(userCharacter);
+
+	if ( !numFieldFilled || !charFieldFilled ) {
+
+		alert("Both fields MUST be filled.  You will obey.")
+
+	}  else  {
+
+		treeObject[0] = { "height" : userHeight , "character" : userCharacter }
+		// console.log( "treeObject", treeObject );
+		return treeObject;
 	}
+};
+//main tree function
+function tree(specs) {
+	let height = specs[0].height;
+	let character =  specs[0].character;
+	// console.log( height, character );
+	let treeSentence = "";
+	let treeBranch = "";
+	let space = " ";
+	space = space.repeat(height);
+	
 
-}
-
-
+	for (let i = 0; i<height; i++) {
+		
+		treeBranch += character;
+		treeSentence = space + treeBranch
+		console.log( treeSentence ); //this here is the money!!
+		treeBranch += character;
+		space = space.replace(' ','');
+	}
+};
 
 //get grow button from the DOM
 let growButton = document.getElementById('grow-btn');
@@ -49,40 +80,44 @@ let growButton = document.getElementById('grow-btn');
 //event handler to make grow button work
 growButton.addEventListener("click", function(click){
 	click.preventDefault();
-	console.log( "clickhandler working!" );
+	let treeSpecs = fillTreeObject();
+	tree(treeSpecs);
 
-// define the tree object and fill with the appropriate data
-let treeObject = {};
-let userHeight = getNumber();
-let userCharacter = getCharacter();
-treeObject[0] = { "height" : userHeight , "character" : userCharacter }
-console.log( "treeObject", treeObject );
 });
 
 //event handler to make return key work
 let userNumberField = document.getElementById("tree_number");
-userNumberField.addEventListener("keyup", function(event){
+userNumberField.addEventListener("keypress", function(event){
 	if (event.which === 13) {
-		console.log( "return listener on NUMBER field WORKING" );
+		
 	}
+
+
 });
 
 //event handler to make return key work
 let userCharacterField = document.getElementById("tree_character");
-userCharacterField.addEventListener("keyup", function(event){
+userCharacterField.addEventListener("keypress", function(event){
 	if (event.which === 13) {
-		console.log( "return listener on CHARACTER field WORKING" );
+
 	}
 });
+
+
+
+
+
+
+
 
 // Create a tree function that should build a pine tree out of a character in the Chrome dev tools console.
 // It accepts a single object as an argument. The object should have two key/value pairs.
 
-// A key that specifies the height of the pine tree.
-// The value for the height of the tree should be from user input in a <input type="text"> field in the DOM.
-// A key that specifies which character to use to build the pine tree.
-// The character to use should be from user input in a <input type="text"> field in the DOM.
-// Once the user enters in a number, and a character, the user can either then just press the enter key (as long as the cursor is in one of the input fields), or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
+// DONE -A key that specifies the height of the pine tree.
+// DONE he value for the height of the tree should be from user input in a <input type="text"> field in the DOM.
+// DONE -A key that specifies which character to use to build the pine tree.
+// DONE -The character to use should be from user input in a <input type="text"> field in the DOM.
+// DONE Once the user enters in a number, and a character, the user can either then just press the enter key (as long as the cursor is in one of the input fields), or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
 
 // If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value.
 
@@ -90,12 +125,10 @@ userCharacterField.addEventListener("keyup", function(event){
 
 // Example
 
-// Here's what the pine tree should look like when you specify a height of 7, and use the asterisk character.
+// Here's what the pine tree should look like when you specify a height of 5, and use the asterisk character.
 
-//       *
-//      ***
-//     *****
-//    *******
+//       *      =  	1
+//      ***  		1 + 2
+//     *****		3 + 2
+//    *******		5 + 2
 //   *********
-//  ***********
-// *************
